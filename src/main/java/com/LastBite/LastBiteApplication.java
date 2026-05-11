11e5ab0c5b2e78492ce34d49ef1f1c9667db9f6f
Võ Hoàng Minh <minh7n3@gmail.com>
2026-05-11 11:01:27 +0700

    chore: initial project scaffold (Spring Boot 4.0.6)

---
+++ src/main/java/com/LastBite/LastBiteApplication.java
@@ -0,0 +1,13 @@
+package com.LastBite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastBiteApplication.class, args);
+	}
+
+}
